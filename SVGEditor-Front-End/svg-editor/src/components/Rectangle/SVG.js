57750e89b5f4f90ed5b22259738306b615fdcd4e
malikhurem01 <malikhuremovic01@gmail.com>
2024-10-10 23:52:39 +0200

    Base SVG element

--- SVGEditor-Front-End/svg-editor/src/components/Rectangle/SVG.js
+++ SVGEditor-Front-End/svg-editor/src/components/Rectangle/SVG.js
@@ -1,30 +1,42 @@
 import React from "react";
-import classes from "./Rectangle.module.css";
-import ResizeCircle from "./ResizeCircle";
+import Handles from "./Handles";
 
-const SVG = ({ clicked, handleRectangleClick }) => {
+const SVG = ({
+  handleMouseMove,
+  handleMouseUp,
+  handleMouseDown,
+  x,
+  y,
+  width,
+  height,
+}) => {
   return (
-    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
-      <rect
-        className={classes.rectangle}
-        onClick={handleRectangleClick}
-        width="150"
-        height="100"
-        x="25"
-        y="50"
-        fill="white"
-        stroke="black"
-        strokeWidth="1"
-      />
-      {clicked && (
-        <React.Fragment>
-          {/* Circle at the middle of the upper border */}
-          <ResizeCircle type="height" cx="100" cy="50" />
-          {/* Circle at the middle of the right border */}
-          <ResizeCircle type="width" cx="175" cy="100" />
-        </React.Fragment>
-      )}
-    </svg>
+    <React.Fragment>
+      <svg
+        width="100%"
+        height="75vh"
+        style={{ paddingBottom: "10" }}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
+        <rect
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          fill="lightblue"
+          stroke="black"
+          strokeWidth="2"
+        />
+        <Handles
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          handleMouseDown={handleMouseDown}
+        />
+      </svg>
+    </React.Fragment>
   );
 };
 
