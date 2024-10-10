baeb440ea82fdce8ba9f6537f277e90ee18b3c45
malikhurem01 <malikhuremovic01@gmail.com>
2024-10-10 23:51:50 +0200

    Refactored approach to SVG resizing

--- SVGEditor-Front-End/svg-editor/src/components/Rectangle/Rectangle.js
+++ SVGEditor-Front-End/svg-editor/src/components/Rectangle/Rectangle.js
@@ -1,23 +1,59 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import SVG from "./SVG";
-import INITIAL_CONFIGURATION from "./Configuration";
 
-const RectangleSVG = () => {
-  const [isClicked, setIsClicked] = useState(false);
-  const [rectangle, setRectangle] = useState(INITIAL_CONFIGURATION);
-  const handleClick = () => {
-    //toggle rectangle click
-    setIsClicked((prevState) => !prevState);
+const Rectangle = () => {
+  const [resizeHandle, setResizeHandle] = useState(null);
+  const [params, setParams] = useState({
+    x: 50,
+    y: 50,
+    width: 200,
+    height: 100,
+  });
+
+  const { x, y, width, height } = params;
+  const handleMouseDown = (e, corner) => {
+    e.preventDefault();
+    setResizeHandle(corner);
   };
 
+  const handleMouseUp = () => {
+    setResizeHandle(null);
+  };
+
+  const handleMouseMove = (e) => {
+    if (resizeHandle) {
+      const newParams = { width, height, x, y };
+      if (resizeHandle === "south-east") {
+        newParams.width = e.clientX - x;
+        newParams.height = e.clientY - y;
+      } else if (resizeHandle === "south-west") {
+        newParams.width = width - (e.clientX - x);
+        newParams.x = e.clientX;
+        newParams.height = e.clientY - y;
+      } else if (resizeHandle === "north-east") {
+        newParams.height = height - (e.clientY - y);
+        newParams.y = e.clientY;
+        newParams.width = e.clientX - x;
+      } else if (resizeHandle === "north-west") {
+        newParams.height = height - (e.clientY - y);
+        newParams.y = e.clientY;
+        newParams.width = width - (e.clientX - x);
+        newParams.x = e.clientX;
+      }
+      setParams(newParams);
+    }
+  };
   return (
     <SVG
-      width={rectangle.WIDTH}
-      height={rectangle.HEIGHT}
-      clicked={isClicked}
-      handleRectangleClick={handleClick}
+      handleMouseMove={handleMouseMove}
+      handleMouseUp={handleMouseUp}
+      handleMouseDown={handleMouseDown}
+      x={x}
+      y={y}
+      width={width}
+      height={height}
     />
   );
 };
 
-export default RectangleSVG;
+export default Rectangle;
