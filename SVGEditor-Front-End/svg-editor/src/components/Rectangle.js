89a35a45e74a2d2ac34919418a0983038135ae0c
malikhurem01 <malikhuremovic01@gmail.com>
2024-10-10 10:06:06 +0200

    Added Rectangle component

---
+++ SVGEditor-Front-End/svg-editor/src/components/Rectangle.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const RectangleSVG = () => {
+  return (
+    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+      <rect
+        width="150"
+        height="100"
+        x="25"
+        y="50"
+        fill="none"
+        stroke="black"
+        strokeWidth="1"
+      />
+    </svg>
+  );
+};
+
+export default RectangleSVG;
