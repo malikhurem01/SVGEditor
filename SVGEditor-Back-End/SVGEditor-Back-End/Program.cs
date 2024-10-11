5071b565103de89f017c768bd5ad8169fbecfbb8
malikhurem01 <malikhuremovic01@gmail.com>
2024-10-11 02:20:09 +0200

    Service and JSON configuration added to builder

--- SVGEditor-Back-End/SVGEditor-Back-End/Program.cs
+++ SVGEditor-Back-End/SVGEditor-Back-End/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using SVGEditor_Back_End.DTOs;
+using SVGEditor_Back_End.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IConfigurationService, ConfigurationService>();
+builder.Configuration
+    .AddJsonFile("rectangleSettings.json", optional: false, reloadOnChange: false);
+builder.Services.Configure<RectangleSettings>(builder.Configuration.GetSection("Parameters"));
+
 
 var app = builder.Build();
 
@@ -12,6 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+var config = app.Services.GetRequiredService<IConfiguration>();
 
 app.UseHttpsRedirection();
 
