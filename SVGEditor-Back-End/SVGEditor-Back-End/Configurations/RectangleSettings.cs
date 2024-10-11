fb37183b1d26b2f111165b6859906bd2ebded2f2
malikhurem01 <malikhuremovic01@gmail.com>
2024-10-11 02:19:18 +0200

    Class for loading JSON configuration

---
+++ SVGEditor-Back-End/SVGEditor-Back-End/Configurations/RectangleSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace SVGEditor_Back_End.DTOs
+{
+    public class RectangleSettings
+    {
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+    }
+}
