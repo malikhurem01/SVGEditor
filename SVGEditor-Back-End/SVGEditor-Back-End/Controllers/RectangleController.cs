5509b62c1c308d72e9fd1536940bcec20a611341
malikhurem01 <malikhuremovic01@gmail.com>
2024-10-11 02:20:31 +0200

    Rectangle controller added

---
+++ SVGEditor-Back-End/SVGEditor-Back-End/Controllers/RectangleController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SVGEditor_Back_End.DTOs;
+using SVGEditor_Back_End.Services;
+
+namespace SVGEditor_Back_End.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RectangleController : Controller
+    {
+        private readonly IConfigurationService _configurationService;
+        public RectangleController(IConfigurationService configurationService)
+        {
+            _configurationService = configurationService;
+        }
+
+        [HttpGet]
+        public ActionResult<GetRectangleSettings> Get() {
+         return Ok(_configurationService.GetRectangleSettings());
+        }
+
+        [HttpPatch]
+        public async Task<ActionResult<string>> ModifyRectangle(PatchRectangleSettings settings, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return Ok(await _configurationService.ModifyRectangleSettings(settings));
+        }
+    }
+}
