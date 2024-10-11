11fe1f2914b76a307adaa03000a120ceacfcb1a0
malikhurem01 <malikhuremovic01@gmail.com>
2024-10-11 02:20:55 +0200

    Added DTOs for Get and Patch HTTPS requests

---
+++ SVGEditor-Back-End/SVGEditor-Back-End/DTOs/GetRectangleSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace SVGEditor_Back_End.DTOs
+{
+    public class GetRectangleSettings
+    {
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+    }
+}
---
+++ SVGEditor-Back-End/SVGEditor-Back-End/DTOs/PatchRectangleSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace SVGEditor_Back_End.DTOs
+{
+    public class PatchRectangleSettings
+    {
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+    }
+}
